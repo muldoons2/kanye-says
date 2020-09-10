69bf7705a8c5296ad4bfa1350ac4905ebe72309b
muldoons2 <59346412+muldoons2@users.noreply.github.com>
2020-09-10 18:50:33 -0400

    Add files via upload

---
+++ FlaskApp.py
@@ -0,0 +1,20 @@
+import os
+import requests
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+	ye_quote= requests.get("https://api.kanye.rest").json()['quote']
+    return render_template('kanye.html',value=ye_quote)
+
+# @app.route("/kanye")
+# def home():
+#     ye_quote= requests.get("https://api.kanye.rest").json()['quote']
+#     return render_template('kanye.html',value=ye_quote)
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
+# Credits: https://kanye.rest/
